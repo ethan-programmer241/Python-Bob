922943912e00aa12c6ae09048d148e19881cee68
ethan-programmer241 <230343350+ethan-programmer241@users.noreply.github.com>
2026-09-03 21:41:07 +0930

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+import random
+
+Bob = {
+    "name": "Bob",
+    "age": 1,
+    "iq": 10
+}
+
+def checkForResponse(responseList, userInput):
+    for checkInput in responseList["triggers"]:
+        if len(checkInput.split()) == 1:
+            if checkInput in userInput.lower().split():
+                print(random.choice(responseList["responses"]))
+                return True
+        else:
+            if checkInput in userInput.lower():
+                if checkInput in responseTypes["name"]["triggers"]:
+                    print(f"My name is {Bob['name']}!")
+                    return True
+                else:
+                    print(random.choice(responseList["responses"]))
+                    return True
+
+    return False
+
+responseTypes = {
+    "greetings": {
+        "triggers": ["hello", "hi", "good morning", "good afternoon"],
+        "responses": ["Hey there!", "Hello!", "Good morning!", "Hi!"]
+    },
+    "farewells": {
+        "triggers": ["bye", "cya", "goodbye", "later"],
+        "responses": ["Goodbye!", "See you later!", "Bye!", "See ya!"]
+    },
+    "name": {
+        "triggers": ["what is your name",
+                     "what's your name",
+                     "can i get your name"]
+    }
+}
+
+print("This is Bob")
+print()
+
+while True:
+    userInput = input("What would you like to say to Bob? ")
+
+    for category in responseTypes:
+        if checkForResponse(responseTypes[category], userInput):
+            break
+    else:
+        print("I don't understand.")
+    
+    
