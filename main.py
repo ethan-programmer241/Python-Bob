d09759576fbdd0ac8a88d17c8fccc1b108d92939
ethan-programmer241 <230343350+ethan-programmer241@users.noreply.github.com>
2026-09-06 21:43:49 +0930

    Added memory to Bob

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ import random
 
 Bob = {
     "name": "Bob",
-    "age": 1,
+    "age": 10,
     "iq": 10
 }
 
-def checkForResponse(responseList, userInput, category):
+userInfo = {
+    "name": "",
+}
+
+def rememberUserInfo(userInput):
+    for nameTriggers in userInput.lower():
+        for index, word in enumerate(userInput.lower().split()):
+            if word == "is":
+                break
+        name  = userInput.lower().split()[index + 1:]
+        userInfo["name"] = " ".join(name)
+
+def checkForResponse(responseList, userInput):
     for checkInput in responseList["triggers"]:
         if len(checkInput.split()) == 1:
             if checkInput in userInput.lower().split():
@@ -14,19 +26,22 @@ def checkForResponse(responseList, userInput, category):
                 return True
         else:
             if checkInput in userInput.lower():
-                if category == "name":
-                    print(f"My name is {Bob[responseList['bobName']]}!")
+                if "data" in responseList:
+                    print(responseList['response'].format(data=Bob[responseList["data"]]))
                     return True
                 else:
                     print(random.choice(responseList["responses"]))
                     return True
-
     return False
+
+nameTriggers = [
+    "my name is"
+]
 
 responseTypes = {
     "greetings": {
         "triggers": ["hello", "hi", "good morning", "good afternoon"],
-        "responses": ["Hey there!", "Hello!", "Good morning!", "Hi!"]
+        "responses": ["Hey there!", "Hello!", "Howdy!", "Hi!"]
     },
     "farewells": {
         "triggers": ["bye", "cya", "goodbye", "later"],
@@ -36,7 +51,20 @@ responseTypes = {
         "triggers": ["what is your name",
                      "what's your name",
                      "can i get your name"],
-        "bobName": "name"
+        "response": "My name is {data}!",
+        "data": "name"
+    },
+    "age": {
+        "triggers": ["how old are you",
+                     "what is your age"],
+        "response": "I am {data} years old!",
+        "data": "age"
+    },
+    "iq": {
+        "triggers": ["how smart are you",
+                     "what is your iq"],
+        "response": "I have {data} IQ!",
+        "data": "iq"
     }
 }
 
@@ -44,12 +72,14 @@ print("This is Bob")
 print()
 
 while True:
-    userInput = input("What would you like to say to Bob? ")
+    userInput = input("What would you like to say? ")
+
+    rememberUserInfo(userInput)
 
     for category in responseTypes:
-        if checkForResponse(responseTypes[category], userInput, category):
+        if checkForResponse(responseTypes[category], userInput):
             break
     else:
         print("I don't understand.")
-    
-    
+
+    print(userInfo["name"])
